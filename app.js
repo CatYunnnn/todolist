a3982c3a53e7d056300d2f8cd7c22de8763fece2
CatYunnnn <marco20001114@gmail.com>
2023-03-26 14:22:18 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
+const Todo = require("./models/todo");
 const app = express();
 app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
@@ -8,7 +9,10 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 app.get("/", (req, res) => {
-  res.render("index");
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then((todos) => res.render("index", { todos })) // 將資料傳給 index 樣板
+    .catch((error) => console.error(error));
 });
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
