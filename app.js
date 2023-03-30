9e9985c83ae38f0f6a8ece783e60e97cca13407a
CatYunnnn <marco20001114@gmail.com>
2023-03-30 18:13:24 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ if (process.env.NODE_ENV !== "production") {
 app.get("/", (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ _id: "asc" })
     .then((todos) => res.render("index", { todos })) // 將資料傳給 index 樣板
     .catch((error) => console.log(error));
 });
